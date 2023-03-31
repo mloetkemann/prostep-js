63a02443444e804ab494bc67b0d464e104cad7bd
mloetkemann <m.loetkemann@gmail.com>
2023-03-31 20:23:22 +0200

    fix: remove Promise.any

--- src/lib/fileUtil.ts
+++ src/lib/fileUtil.ts
@@ -6,18 +6,23 @@ const logger = Logger.getLogger("TaskImporter");
 
 async function tryImport(path: string): Promise<any> {
     logger.verbose(`Try import ${path}`);
-    const taskCls = await import(path);
-    logger.verbose(`Found ${path}`);
-    return taskCls.default;
+    try {
+        const taskCls = await import(path);
+    
+        logger.verbose(`Found ${path}`);
+        return taskCls.default;
+    }catch(e) {
+        return;
+    }
 }
 
-export function instantiateTask(relativePath: string): Promise<any> {
-
+export async function instantiateTask(relativePath: string): Promise<any> {
+        let result;
         let paths = [relativePath];
-        paths = paths.concat(module.paths);
-        const accessPromises = paths.map(p => {
-            let path = join(dirname(p), relativePath);
-            return tryImport(path);
-        });
-        return Promise.any(accessPromises);
+        paths = paths.concat(module.paths.map(p => join(dirname(p), relativePath)));
+        for(const path of paths) {
+            result = await tryImport(path);
+            if(result) break;
+        }
+        return result;
 }
